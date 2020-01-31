0da07bf0f6f3b866497a0d313f019a365e48defb
enbot <nimbotelho@gmail.com>
2020-01-31 00:52:42 -0300

    gulp build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,46 @@
-"use strict";
+"use strict"
 
-// const gulp = require('gulp')
-// const terser = require('gulp-terser')
+const gulp = require('gulp')
+const terser = require('gulp-terser')
+const minifycss = require("gulp-minify-css")
+const minifyhtml = require("gulp-minify-html")
+const browserSync = require("browser-sync").create()
 
-// gulp.task("js", function () {
-//     return gulp.src([
-//         './js/*.js',
-//         './js/**/**.js',
-//     ])
-//         .pipe(terser())
-//         .pipe(gulp.dest('dist'))
-// })
+gulp.task("html", function () {
+    return gulp.src('./exports/*.html')
+        .pipe(minifyhtml())
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.reload({ stream: true }))
+})
 
-let gulp = require("gulp");
-// let rename = require("gulp-rename");
-let uglify = require('gulp-uglify-es').default;
- 
+gulp.task('css', () => {
+    return gulp.src('./exports/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.reload({ stream: true }))
+})
+
 gulp.task("js", function () {
-    return gulp.src([
-                './js/*.js',
-                './js/**/**.js',
-            ])
-        // .pipe(rename("bundle.min.js"))
-        .pipe(uglify(/* options */))
-        .pipe(gulp.dest("dist/"));
-});
+    return gulp.src("./exports/*.js")
+        .pipe(terser())
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.reload({ stream: true }))
+})
+
+gulp.task("watchfiles", function () {
+
+    browserSync.init({
+        open: "external",
+        port: 4200,
+        server: { baseDir: './build/' },
+        injectChanges: true,
+        ui: { port: 4201 }
+    })
+
+    gulp.watch("./exports/*.html", gulp.series("html"))
+    gulp.watch("./exports/*.css", gulp.series("css"))
+    gulp.watch("./exports/*.js", gulp.series("js"))
+})
+
+gulp.task('build', gulp.series(['html', 'css', 'js']))
+gulp.task('run', gulp.series(['build', 'watchfiles'])) 
